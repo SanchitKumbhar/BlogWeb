61eb24953eac1c9fd969512f60336e0f6e293217
kritzxd8 <korikriti4@gmail.com>
2024-06-26 16:04:34 +0530

    blog page added & hamburger added

--- static/assets/js/kritika js/index.js
+++ static/assets/js/kritika js/index.js
@@ -1,21 +1,26 @@
-let viewCount = 0;
+// let viewCount = 0;
 
-// const viewCountDiv = document.querySelectorAll(".view-blog-btn");
-// const viewCountPara = document.getElementById("#view-count");
-const eachBlog = document.querySelectorAll(".blog-post");
+// const eachBlog = document.querySelectorAll(".blog-post");
 
 
-eachBlog.forEach((eachBlog) => {
-    // eachBlog.addEventListener("click", () => {
-    const viewCountDiv = document.querySelector(".view-blog-btn");
-    const viewCountPara = document.querySelector(".view-count");
+// eachBlog.forEach((eachBlog) => {
 
-    viewCountDiv.addEventListener("click", () => {
-        viewCount= viewCount+1;
-        console.log("1 view", viewCount);
-        // changingViewCount(viewCountDiv);
+//     const viewCountDiv = document.querySelector(".view-blog-btn");
+//     const viewCountPara = document.querySelector(".view-count");
 
-        viewCountPara.innerHTML = viewCount;
-    });
-});
+//     viewCountDiv.addEventListener("click", () => {
+//         viewCount= viewCount+1;
+//         console.log("1 view", viewCount);
 
+
+//         viewCountPara.innerHTML = viewCount;
+//     });
+// });
+
+const burger = document.querySelector('.burger');
+const navList = document.querySelector('.off-screen-menu');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navList.classList.toggle('active');
+})
